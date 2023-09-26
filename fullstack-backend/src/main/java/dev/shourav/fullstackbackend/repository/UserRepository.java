bfb179ed3c079dbc907be4f35dcfd0817e861f81
ShouravMolla99 <56232124+ShouravMolla99@users.noreply.github.com>
2023-09-26 22:01:21 +0600

    backend with java done

---
+++ fullstack-backend/src/main/java/dev/shourav/fullstackbackend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package dev.shourav.fullstackbackend.repository;
+
+import dev.shourav.fullstackbackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
